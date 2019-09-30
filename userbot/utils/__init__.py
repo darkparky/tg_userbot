3e0bc12c0ad91ad92006c257c928a79c05db737d
Chris Watson <cawatson1993@gmail.com>
2019-09-29 22:11:48 -0700

    Fix argparsing

--- userbot/utils/__init__.py
+++ userbot/utils/__init__.py
@@ -1,28 +1,32 @@
 from re import findall, match
+from typing import Optional
 
 from telethon.tl.types import MessageEntityMentionName, ChannelParticipantsAdmins
 
 
-def parse_arguments(message):
+def parse_arguments(message: str) -> (dict, str):
     options = {}
 
     # Handle boolean values
-    for opt in findall(r"\s+([.!]\S+)\s+", message):
+    for opt in findall(r'\s+([.!]\S+)', message):
         if opt[0] == '.':
             options[opt[1:]] = True
         elif opt[0] == '!':
             options[opt[1:]] = False
         message = message.replace(opt, '')
-    
+
     # Handle key/value pairs
-    for opt in findall(r"\s+(\S+):(\S+)\s+", message):
+    for opt in findall(r'\s+(\S+):(\S+)', message):
         key, value = opt
-        if value.isnumeric(): value = int(value)
-        elif match(r"[Tt]rue|[Ff]alse", value): match(r"[Tt]rue", value)
+        if value.isnumeric():
+            value = int(value)
+        elif match(r'[Tt]rue|[Ff]alse', value):
+            match(r'[Tt]rue', value)
         options[key] = value
         message = message.replace(':'.join(opt), '')
 
-    return (options, message.strip())
+    return options, message.strip()
+
 
 def freeze(d):
     if isinstance(d, dict):
@@ -31,9 +35,11 @@ def freeze(d):
         return tuple(freeze(value) for value in d)
     return d
 
+
 def extract_urls(message):
     matches = findall(r'(https?://\S+)', str(message))
     return list(matches)
+
 
 async def get_user_from_event(event):
     """ Get the user from argument or replied message. """
