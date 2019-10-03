23a0974d4dafd014596bf7841435afb920eb9000
Chris Watson <cawatson1993@gmail.com>
2019-10-03 11:34:38 -0600

    Fix lfy

--- userbot/modules/misc/lfy.py
+++ userbot/modules/misc/lfy.py
@@ -36,7 +36,7 @@ async def let_me_google_that_for_you(e):
         reply_message = await e.get_reply_message()
         query = message if message else reply_message.text
 
-        reply_text = f"Hmm, [{provider['message'] % query}]({provider['source']})"
+        reply_text = f"Hmm, [{provider['message']}]({provider['source'] % query})"
         await e.edit(reply_text, reply_to=reply_message)
 
 add_help_item(
