21c1a08372662737d6f0cdf1f877ec41d9235e52
Chris Watson <cawatson1993@gmail.com>
2019-10-12 13:26:39 -0600

    Update formatting

--- userbot/modules/misc/b64.py
+++ userbot/modules/misc/b64.py
@@ -2,19 +2,23 @@ import pybase64
 
 from ..help import add_help_item
 from userbot.events import register
+from userbot.utils.tgdoc import *
 
 
 @register(outgoing=True, pattern=r"^\.b64\s+(en|de)(?:\s+(.*))?")
-async def endecrypt(query):
+async def endecrypt(e):
     """ For .b64 command, find the base64 encoding of the given string. """
-    reply_message = await query.get_reply_message()
-    text = query.pattern_match.group(2) or reply_message.text
-    if query.pattern_match.group(1) == "en":
+    reply_message = await e.get_reply_message()
+    text = e.pattern_match.group(2) or reply_message.message
+    output = Section(SubSection(Bold("Input"), Code(text), indent=0), indent=0)
+    if e.pattern_match.group(1) == "en":
         lething = str(pybase64.b64encode(bytes(text, "utf-8")))[2:]
-        await query.reply("Encoded: `" + lething[:-1] + "`")
+        output += SubSection(Bold("Encoded"), Code(lething[:-1]), indent=0)
     else:
         lething = str(pybase64.b64decode(bytes(text, "utf-8"), validate=True))[2:]
-        await query.reply("Decoded: `" + lething[:-1] + "`")
+        output += SubSection(Bold("Decoded"), Code(lething[:-1]), indent=0)
+    await e.edit(str(output))
+
 
 add_help_item(
     ".b64",
